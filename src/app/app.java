e08f78bb233628cc9b6d8c9a2f8a8455e6d3812e
Jai_Vang <jaivang0387@gmail.com>
2024-10-16 10:29:11 -0400

    Starting the process of modularizing code

--- src/app/app.java
+++ src/app/app.java
@@ -15,93 +15,103 @@ public class app {
         boolean keepRunning = true;
 
         while (keepRunning) {
-            System.out.print("\nEnter the size of the array: ");
-            int size = scanner.nextInt();
+            int[] array = getArrayInput(scanner);
 
-            // Ask user for the range of the array elements
-            System.out.print("Enter the minimum value for the array: ");
-            int min = scanner.nextInt();
-
-            System.out.print("Enter the maximum value for the array: ");
-            int max = scanner.nextInt();
-
-            // Generate the array with the specified size and range
-            int[] array = generateRandomArray(size, min, max);
-
-            System.out.println("\nDo you want to sort or search the array?\n");
-            System.out.println("1. Sort the array");
-            System.out.println("2. Search in the array");
-            System.out.println("0. Exit");
-
-            int operationChoice = scanner.nextInt();
-
+            int operationChoice = getOperationChoice(scanner);
             if (operationChoice == 0) {
                 System.out.println("Exiting the program...");
                 keepRunning = false;
-                break;
-            }
-
-            if (operationChoice == 1) {
-                System.out.println("\nChoose a sorting algorithm:");
-                System.out.println("1. Bubble Sort");
-                System.out.println("2. Merge Sort");
-                System.out.println("3. Quick Sort");
-                System.out.println("4. Selection Sort");
-                System.out.println("5. Insertion Sort");
-                System.out.println("6. Heap Sort");
-                System.out.println("7. Shell Sort");
-
-                int choice = scanner.nextInt();
-                SortingAlgorithmInterface sortingAlgorithm = getSortingAlgorithm(choice);
-
-                long startTime = System.currentTimeMillis();
-                sortingAlgorithm.sort(array);
-                long endTime = System.currentTimeMillis();
-                long executionTime = endTime - startTime;
-
-                System.out.println("Sorted Array: " + Arrays.toString(array));
-                System.out.println("Execution Time: " + executionTime + " milliseconds");
-
+            } else if (operationChoice == 1) {
+                performSorting(scanner, array);
             } else if (operationChoice == 2) {
-                System.out.println("\nChoose a searching algorithm:");
-                System.out.println("1. Linear Search");
-                System.out.println("2. Binary Search");
-                System.out.println("3. Exponential Search");
-                System.out.println("4. Fibonacci Search");
-                System.out.println("5. Hashing Search");
-                System.out.println("6. Interpolation Search");
-                System.out.println("7. Jump Search");
-                System.out.println("8. Ternary Search");
-
-                int choice = scanner.nextInt();
-                SearchingAlgorithmInterface searchingAlgorithm = getSearchingAlgorithm(choice);
-
-                System.out.print("\nEnter the target element to search for: ");
-                int target = scanner.nextInt();
-
-                long startTime = System.currentTimeMillis();
-
-                // Find all occurrences and indices
-                int[] indices = findOccurrences(array, target);
-                if (indices.length > 0) {
-                    System.out.println("\nElement " + target + " found at indices: " + Arrays.toString(indices) + "\n");
-                    System.out.println("Element " + target + " was found " + indices.length + " time(s) in the array.");
-                } else {
-                    System.out.println("Element not found in the array.");
-                }
-
-                searchingAlgorithm.search(array, target);
-
-                long endTime = System.currentTimeMillis();
-                long executionTime = endTime - startTime;
-
-                System.out.println("Execution Time: " + executionTime + " milliseconds");
+                performSearching(scanner, array);
             }
         }
 
         scanner.close();
     }
 
+    // Method to get the array size and range input from the user
+    public static int[] getArrayInput(Scanner scanner) {
+        System.out.print("\nEnter the size of the array: ");
+        int size = scanner.nextInt();
+
+        System.out.print("Enter the minimum value for the array: ");
+        int min = scanner.nextInt();
+
+        System.out.print("Enter the maximum value for the array: ");
+        int max = scanner.nextInt();
+
+        return generateRandomArray(size, min, max);
+    }
+
+    // Method to ask for the user's choice of operation (sort or search)
+    public static int getOperationChoice(Scanner scanner) {
+        System.out.println("\nDo you want to sort or search the array?\n");
+        System.out.println("1. Sort the array");
+        System.out.println("2. Search in the array");
+        System.out.println("0. Exit");
+        return scanner.nextInt();
+    }
+
+    // Method to handle sorting operations
+    public static void performSorting(Scanner scanner, int[] array) {
+        System.out.println("\nChoose a sorting algorithm:");
+        System.out.println("1. Bubble Sort");
+        System.out.println("2. Merge Sort");
+        System.out.println("3. Quick Sort");
+        System.out.println("4. Selection Sort");
+        System.out.println("5. Insertion Sort");
+        System.out.println("6. Heap Sort");
+        System.out.println("7. Shell Sort");
+
+        int choice = scanner.nextInt();
+        SortingAlgorithmInterface sortingAlgorithm = getSortingAlgorithm(choice);
+
+        long startTime = System.currentTimeMillis();
+        sortingAlgorithm.sort(array);
+        long endTime = System.currentTimeMillis();
+        long executionTime = endTime - startTime;
+
+        System.out.println("Sorted Array: " + Arrays.toString(array));
+        System.out.println("Execution Time: " + executionTime + " milliseconds");
+    }
+
+    // Method to handle searching operations
+    public static void performSearching(Scanner scanner, int[] array) {
+        System.out.println("\nChoose a searching algorithm:");
+        System.out.println("1. Linear Search");
+        System.out.println("2. Binary Search");
+        System.out.println("3. Exponential Search");
+        System.out.println("4. Fibonacci Search");
+        System.out.println("5. Hashing Search");
+        System.out.println("6. Interpolation Search");
+        System.out.println("7. Jump Search");
+        System.out.println("8. Ternary Search");
+
+        int choice = scanner.nextInt();
+        SearchingAlgorithmInterface searchingAlgorithm = getSearchingAlgorithm(choice);
+
+        System.out.print("\nEnter the target element to search for: ");
+        int target = scanner.nextInt();
+
+        long startTime = System.currentTimeMillis();
+        int[] indices = findOccurrences(array, target);
+        if (indices.length > 0) {
+            System.out.println("\nElement " + target + " found at indices: " + Arrays.toString(indices) + "\n");
+            System.out.println("Element " + target + " was found " + indices.length + " time(s) in the array.");
+        } else {
+            System.out.println("Element not found in the array.");
+        }
+
+        searchingAlgorithm.search(array, target);
+        long endTime = System.currentTimeMillis();
+        long executionTime = endTime - startTime;
+
+        System.out.println("Execution Time: " + executionTime + " milliseconds");
+    }
+
+    // Method to get the sorting algorithm based on the user's choice
     public static SortingAlgorithmInterface getSortingAlgorithm(int choice) {
         switch (choice) {
             case 1:
@@ -123,6 +133,7 @@ public class app {
         }
     }
 
+    // Method to get the searching algorithm based on the user's choice
     public static SearchingAlgorithmInterface getSearchingAlgorithm(int choice) {
         switch (choice) {
             case 1:
@@ -171,7 +182,6 @@ public class app {
         }
 
         System.out.println("\nGenerated Array: " + Arrays.toString(array));
-
         return array;
     }
 }
